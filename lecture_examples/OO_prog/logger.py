e8860dd22522e61b15ad128fa635b92fe6626dc3
MarcoFava <marco.fava@salesianiverona.it>
2025-05-20 14:43:21 +0200

    cleanup

--- lecture_examples/OO_prog/logger.py
+++
@@ -1,7 +0,0 @@
-verbosity = "INFO"
-
-def info(message):
-    if verbosity == "INFO":
-        print('INFO:', message)
-    if verbosity == "WARNING":
-        print('WARNING: ', message)
--- lecture_examples/OO_prog/system1.py
+++
@@ -1,8 +0,0 @@
-position = [0.0, 0.0, 0.0]
-species = 'Ar'
-
-def compute():
-    # do something with (global) position and species
-    print(species)
-    # pass
-
